049604d65dd817c4a477be3465b28f9d918f91b0
kimkiyun <kiyoon9390@gmail.com>
2021-03-24 20:55:08 +0900

    [CREATE] member Repository

---
+++ src/main/java/inflearn/inflearn/domain/Member.java
@@ -0,0 +1,13 @@
+package inflearn.inflearn.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Member {
+
+    private Long id;
+    private String name;
+
+}
---
+++ src/main/java/inflearn/inflearn/repository/MemoryMemberRepository.java
@@ -0,0 +1,35 @@
+package inflearn.inflearn.repository;
+
+import inflearn.inflearn.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static Long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny(); //돌면서 하나라도 찾으면 반환을 하고 아니면 optional에 null이 포함되어 반환된
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
